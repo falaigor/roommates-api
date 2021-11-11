b51ea018563658ebfef281222b522b129792b4be
Igor Santos <falaigors@gmail.com>
2021-11-11 01:36:07 -0300

    fix build

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,26 @@
-import { serverHttp } from "./app";
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+import { Server } from "socket.io";
+import { router } from "./routes";
+
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+app.use(router);
 
 serverHttp.listen(4000, () => console.log("Server is runing on PORT 4000"));
--- src/services/CreateRoomService.ts
+++ src/services/CreateRoomService.ts
@@ -1,5 +1,4 @@
 import prismaClient from "../prisma";
-import { io } from "../app";
 
 class CreateRoomService {
   async execute(
@@ -25,17 +24,6 @@ class CreateRoomService {
         images: true,
       },
     });
-
-    const infoWS = {
-      title: room.title,
-      description: room.description,
-      price: room.price,
-      latitude: room.latitude,
-      longitude: room.longitude,
-      images: room.images,
-    };
-
-    io.emit("new_room", infoWS);
 
     return room;
   }
